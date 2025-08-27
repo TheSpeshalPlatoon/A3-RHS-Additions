751a6b15d4b1cb9fdc75548a6d712c0d9ca047c1
Goosey <putralvaro376@gmail.com>
2025-08-27 13:32:05 +0700

    Sight fixed

--- tsp_rhs_m4/config.cpp
+++ tsp_rhs_m4/config.cpp
@@ -6,6 +6,8 @@ class CfgWeapons {
 	class rhs_acc_pointer;
 	class rhs_weap_m4_carryhandle;
 	class rhs_weap_m4a1_carryhandle;
+	class rhs_weap_m4;
+	class rhs_weap_m4a1;
 
 
 	class rhs_acc_m4_handguard: rhs_acc_bipod {displayName = "Plastic Handguard"; model = "\tsp_rhs_m4\handguard.p3d"; descriptionShort = "Standard plastic M4 handguard";};
@@ -39,12 +41,17 @@ class CfgWeapons {
 		class WeaponSlotsInfo: WeaponSlotsInfo {class pointerSlot {linkProxy = "\a3\data_f\proxies\weapon_slots\SIDE"; class compatibleItems {rhs_acc_m4_car_pointer = 1;};};};
 	};
 
-	class rhs_weap_m4_moe: rhs_weap_m4_handguard {
+	class rhs_weap_m4_moe: rhs_weap_m4 {
 		baseWeapon = "rhs_weap_m4_moe"; displayName = "M4 (Magpul MOE)"; magazines[] = {"rhs_mag_30Rnd_556x45_M855A1_PMAG"}; model = "\rhsusf\addons\rhsusf_weapons\M4\m4_ris_mstock.p3d";
 		hiddenSelectionsTextures[] = {"\tsp_rhs_weapon\tex\m4_noris.paa", "\rhsusf\addons\rhsusf_weapons\m4\data\magazine_co.paa", "\rhsusf\addons\rhsusf_weapons\m4\data\m4acc_co.paa", "\tsp_rhs_weapon\tex\empty.paa", "\rhsusf\addons\rhsusf_weapons\acc\grips\grippod\data\acc_co.paa", "\rhsusf\addons\rhsusf_weapons\m4\data\magpulstock_co.paa"};
+		hiddenSelectionsMaterials[] = {"\rhsusf\addons\rhsusf_weapons\m4\data\m4a1.rvmat", "\rhsusf\addons\rhsusf_weapons\m4\data\magazine.rvmat", "\rhsusf\addons\rhsusf_weapons\m4\data\m4acc_co.rvmat", "\tsp_rhs_weapon\tex\empty.rvmat"};
 		class linkedItems {class linkedItemsUnder {slot = "underBarrelSlot"; item = "rhs_acc_m4_moe";};};
+		class WeaponSlotsInfo: WeaponSlotsInfo {
+			class gripodSlot {}; 
+			class underBarrelSlot {linkProxy = "\A3\data_f_mark\proxies\weapon_slots\UNDERBARREL"; class compatibleItems {rhs_acc_m4_handguard = 1; rhs_acc_m4_moe = 1; rhs_acc_m4_moe_tan = 1;};};
+		};
 	};
-	class rhs_weap_m4_moe_tan: rhs_weap_m4_handguard {
+	class rhs_weap_m4_moe_tan: rhs_weap_m4_moe {
 		baseWeapon = "rhs_weap_m4_moe_tan"; displayName = "M4 (Magpul MOE/Tan)"; magazines[] = {"rhs_mag_30Rnd_556x45_M855A1_PMAG_Tan"};  model = "\rhsusf\addons\rhsusf_weapons\M4\m4_ris_mstock.p3d";
 		hiddenSelectionsTextures[] = {"\tsp_rhs_weapon\tex\m4_noris.paa", "\rhsusf\addons\rhsusf_weapons\m4\data\magazine_co.paa", "\rhsusf\addons\rhsusf_weapons\m4\data\m4acc_co.paa", "\tsp_rhs_weapon\tex\empty.paa", "\rhsusf\addons\rhsusf_weapons\acc\grips\grippod\data\acc_co.paa", "\rhsusf\addons\rhsusf_weapons\m4\data\magpulstock_d_co.paa"};
 		class linkedItems {class linkedItemsUnder {slot = "underBarrelSlot"; item = "rhs_acc_m4_moe_tan";};};
@@ -76,12 +83,17 @@ class CfgWeapons {
 		class WeaponSlotsInfo: WeaponSlotsInfo {class pointerSlot {linkProxy = "\a3\data_f\proxies\weapon_slots\SIDE"; class compatibleItems {rhs_acc_m4_car_pointer = 1;};};};
 	};
 	
-	class rhs_weap_m4a1_moe: rhs_weap_m4a1_handguard {
+	class rhs_weap_m4a1_moe: rhs_weap_m4a1 {
 		baseWeapon = "rhs_weap_m4a1_moe"; displayName = "M4A1 (Magpul MOE)"; magazines[] = {"rhs_mag_30Rnd_556x45_M855A1_PMAG"}; model = "\rhsusf\addons\rhsusf_weapons\M4\m4_ris_mstock.p3d";
 		hiddenSelectionsTextures[] = {"\tsp_rhs_weapon\tex\m4a1_noris.paa", "\rhsusf\addons\rhsusf_weapons\m4\data\magazine_co.paa", "\rhsusf\addons\rhsusf_weapons\m4\data\m4acc_co.paa", "\tsp_rhs_weapon\tex\empty.paa", "\rhsusf\addons\rhsusf_weapons\acc\grips\grippod\data\acc_co.paa", "\rhsusf\addons\rhsusf_weapons\m4\data\magpulstock_co.paa"};
+		hiddenSelectionsMaterials[] = {"\rhsusf\addons\rhsusf_weapons\m4\data\m4a1_actual.rvmat", "\rhsusf\addons\rhsusf_weapons\m4\data\magazine.rvmat", "\rhsusf\addons\rhsusf_weapons\m4\data\m4acc_co.rvmat", "\tsp_rhs_weapon\tex\empty.rvmat"};
 		class linkedItems {class linkedItemsUnder {slot = "underBarrelSlot"; item = "rhs_acc_m4_moe";};};
+		class WeaponSlotsInfo: WeaponSlotsInfo {
+			class gripodSlot {}; 
+			class underBarrelSlot {linkProxy = "\A3\data_f_mark\proxies\weapon_slots\UNDERBARREL"; class compatibleItems {rhs_acc_m4_handguard = 1; rhs_acc_m4_moe = 1; rhs_acc_m4_moe_tan = 1;};};
+		};
 	};
-	class rhs_weap_m4a1_moe_tan: rhs_weap_m4a1_handguard {
+	class rhs_weap_m4a1_moe_tan: rhs_weap_m4a1_moe {
 		baseWeapon = "rhs_weap_m4a1_moe_tan"; displayName = "M4A1 (Magpul MOE/Tan)"; magazines[] = {"rhs_mag_30Rnd_556x45_M855A1_PMAG_Tan"};  model = "\rhsusf\addons\rhsusf_weapons\M4\m4_ris_mstock.p3d";
 		hiddenSelectionsTextures[] = {"\tsp_rhs_weapon\tex\m4a1_noris.paa", "\rhsusf\addons\rhsusf_weapons\m4\data\magazine_co.paa", "\rhsusf\addons\rhsusf_weapons\m4\data\m4acc_co.paa", "\tsp_rhs_weapon\tex\empty.paa", "\rhsusf\addons\rhsusf_weapons\acc\grips\grippod\data\acc_co.paa", "\rhsusf\addons\rhsusf_weapons\m4\data\magpulstock_d_co.paa"};
 		class linkedItems {class linkedItemsUnder {slot = "underBarrelSlot"; item = "rhs_acc_m4_moe_tan";};};
