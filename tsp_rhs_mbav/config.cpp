b473adc6f5315cb108b18df87fb06ecfa5e5af8b
Goosey <putralvaro376@gmail.com>
2026-05-20 17:23:37 +0700

    Of five people, three must pay the pricee

--- tsp_rhs_mbav/config.cpp
+++ tsp_rhs_mbav/config.cpp
@@ -1,19 +1,159 @@
-class CfgPatches {class tsp_rhs_mbav {requiredAddons[] = {"rhs_infantry"}; units[] = {}; skipWhenMissingDependencies = 1;};};
-
-class CfgWeapons {
-    class rhsusf_mbav; class rhsusf_mbav_light; class rhsusf_mbav_rifleman;	class rhsusf_mbav_mg; class rhsusf_mbav_grenadier; class rhsusf_mbav_medic;
-	#define mbav_green hiddenSelections[] = {"camo", "camo2"}; hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_rgr_co.paa", "tsp_rhs_mbav\tex\gear_rgr_co.paa"};
-	#define mbav_multicam hiddenSelections[] = {"camo", "camo2"}; hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_mc_co.paa", "tsp_rhs_mbav\tex\gear_mc_co.paa"};
-	class mbavr: rhsusf_mbav {displayName = "MBAV Ranger"; model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader"; mbav_green};
-	class mbavr_l: rhsusf_mbav_light {displayName = "MBAV Ranger (Light)"; model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader"; mbav_green};
-	class mbavr_r: rhsusf_mbav_rifleman	{displayName = "MBAV Ranger (Rifleman)"; model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader"; mbav_green};
-	class mbavr_mg: rhsusf_mbav_mg {displayName = "MBAV Ranger (Machinegunner)"; model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader"; mbav_green};
-	class mbavr_gl: rhsusf_mbav_grenadier {displayName = "MBAV Ranger (Grenadier)"; model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader"; mbav_green};
-	class mbavr_m: rhsusf_mbav_medic {displayName = "MBAV Ranger (Medic)"; model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader"; mbav_green};
-	//class mbavmc: rhsusf_mbav {displayName = "MBAV Multicam"; model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader"; mbav_multicam};
-	class mbavmc_l: rhsusf_mbav_light {displayName = "MBAV Multicam (Light)"; model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader"; mbav_multicam};
-	class mbavmc_r: rhsusf_mbav_rifleman {displayName = "MBAV Multicam (Rifleman)"; model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader"; mbav_multicam};
-	class mbavmc_mg: rhsusf_mbav_mg	{displayName = "MBAV Multicam (Machinegunner)"; model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";	mbav_multicam};
-	class mbavmc_gl: rhsusf_mbav_grenadier {displayName = "MBAV Multicam (Grenadier)"; model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";	mbav_multicam};
-	class mbavmc_m: rhsusf_mbav_medic {displayName = "MBAV Multicam (Medic)"; model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader"; mbav_multicam};
+class CfgPatches
+{
+	class tsp_rhs_mbav
+	{
+		requiredAddons[] = {"rhs_infantry"};
+		units[] = {};
+		skipWhenMissingDependencies = 1;
+	};
+};
+class CfgWeapons
+{
+	class rhsusf_mbav;
+	class rhsusf_mbav_light;
+	class rhsusf_mbav_rifleman;
+	class rhsusf_mbav_mg;
+	class rhsusf_mbav_grenadier;
+	class rhsusf_mbav_medic;
+	class mbavr: rhsusf_mbav
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_rgr.paa";
+		displayName = "MBAV Ranger";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_rgr_co.paa","tsp_rhs_mbav\tex\gear_rgr_co.paa"};
+	};
+	class mbavr_l: rhsusf_mbav_light
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_rgr.paa";
+		displayName = "MBAV Ranger (Light)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_rgr_co.paa","tsp_rhs_mbav\tex\gear_rgr_co.paa"};
+	};
+	class mbavr_r: rhsusf_mbav_rifleman
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_rgr.paa";
+		displayName = "MBAV Ranger (Rifleman)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_rgr_co.paa","tsp_rhs_mbav\tex\gear_rgr_co.paa"};
+	};
+	class mbavr_mg: rhsusf_mbav_mg
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_rgr.paa";
+		displayName = "MBAV Ranger (Machinegunner)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_rgr_co.paa","tsp_rhs_mbav\tex\gear_rgr_co.paa"};
+	};
+	class mbavr_gl: rhsusf_mbav_grenadier
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_rgr.paa";
+		displayName = "MBAV Ranger (Grenadier)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_rgr_co.paa","tsp_rhs_mbav\tex\gear_rgr_co.paa"};
+	};
+	class mbavr_m: rhsusf_mbav_medic
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_rgr.paa";
+		displayName = "MBAV Ranger (Medic)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_rgr_co.paa","tsp_rhs_mbav\tex\gear_rgr_co.paa"};
+	};
+	class mbavmc_l: rhsusf_mbav_light
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_mc.paa";
+		displayName = "MBAV Multicam (Light)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_mc_co.paa","tsp_rhs_mbav\tex\gear_mc_co.paa"};
+	};
+	class mbavmc_r: rhsusf_mbav_rifleman
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_mc.paa";
+		displayName = "MBAV Multicam (Rifleman)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_mc_co.paa","tsp_rhs_mbav\tex\gear_mc_co.paa"};
+	};
+	class mbavmc_mg: rhsusf_mbav_mg
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_mc.paa";
+		displayName = "MBAV Multicam (Machinegunner)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_mc_co.paa","tsp_rhs_mbav\tex\gear_mc_co.paa"};
+	};
+	class mbavmc_gl: rhsusf_mbav_grenadier
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_mc.paa";
+		displayName = "MBAV Multicam (Grenadier)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_mc_co.paa","tsp_rhs_mbav\tex\gear_mc_co.paa"};
+	};
+	class mbavmc_m: rhsusf_mbav_medic
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_mc.paa";
+		displayName = "MBAV Multicam (Medic)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_mc_co.paa","tsp_rhs_mbav\tex\gear_mc_co.paa"};
+	};
+	class mbavb: rhsusf_mbav
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_blk.paa";
+		displayName = "MBAV Black";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_blk_co.paa","tsp_rhs_mbav\tex\gear_blk_co.paa"};
+	};
+	class mbavb_l: rhsusf_mbav_light
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_blk.paa";
+		displayName = "MBAV Black (Light)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_blk_co.paa","tsp_rhs_mbav\tex\gear_blk_co.paa"};
+	};
+	class mbavb_r: rhsusf_mbav_rifleman
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_blk.paa";
+		displayName = "MBAV Black (Rifleman)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_blk_co.paa","tsp_rhs_mbav\tex\gear_blk_co.paa"};
+	};
+	class mbavb_mg: rhsusf_mbav_mg
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_blk.paa";
+		displayName = "MBAV Black (Machinegunner)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_blk_co.paa","tsp_rhs_mbav\tex\gear_blk_co.paa"};
+	};
+	class mbavb_gl: rhsusf_mbav_grenadier
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_blk.paa";
+		displayName = "MBAV Black (Grenadier)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_blk_co.paa","tsp_rhs_mbav\tex\gear_blk_co.paa"};
+	};
+	class mbavb_m: rhsusf_mbav_medic
+	{
+		picture = "\tsp_rhs_mbav\ui\mbav_blk.paa";
+		displayName = "MBAV Black (Medic)";
+		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_spc_squadleader";
+		hiddenSelections[] = {"camo","camo2"};
+		hiddenSelectionsTextures[] = {"tsp_rhs_mbav\tex\mbav_blk_co.paa","tsp_rhs_mbav\tex\gear_blk_co.paa"};
+	};
+};
+class cfgMods
+{
+	author = "AHHHHHHHHHHHHHHH";
+	timepacked = "1756235702";
 };
--- tsp_rhs_mich/config.cpp
+++ tsp_rhs_mich/config.cpp
@@ -1,6 +1,14 @@
-class CfgPatches {class tsp_rhs_mich {requiredAddons[] = {"rhs_infantry"}; units[] = {}; skipWhenMissingDependencies = 1;};};
-
-class CfgWeapons {
+class CfgPatches
+{
+	class tsp_rhs_mich
+	{
+		requiredAddons[] = {"rhs_infantry"};
+		units[] = {};
+		skipWhenMissingDependencies = 1;
+	};
+};
+class CfgWeapons
+{
 	class rhsusf_mich_bare_norotos;
 	class rhsusf_mich_bare_norotos_headset;
 	class rhsusf_mich_bare_norotos_alt;
@@ -9,43 +17,117 @@ class CfgWeapons {
 	class rhsusf_mich_bare_norotos_arc_headset;
 	class rhsusf_mich_bare_norotos_arc_alt;
 	class rhsusf_mich_bare_norotos_arc_alt_headset;
-
-	/*class rhsusf_mich_bare_norotos_novelcro: rhsusf_mich_bare_norotos {displayName = "MICH 2000 (Norotos/No Velcro)";};
-	class rhsusf_mich_bare_norotos_headset_novelcro: rhsusf_mich_bare_norotos_headset {displayName = "MICH 2000 (Norotos/Headset/No Velcro)";};
-	class rhsusf_mich_bare_norotos_alt_novelcro: rhsusf_mich_bare_norotos_alt {displayName = "MICH 2000 (Norotos/Alt/No Velcro)";};
-	class rhsusf_mich_bare_norotos_alt_headset_novelcro: rhsusf_mich_bare_norotos_alt_headset {displayName = "MICH 2000 (Norotos/Alt/Headset/No Velcro)";};
-	class rhsusf_mich_bare_norotos_arc_novelcro: rhsusf_mich_bare_norotos_arc {
-		displayName = "MICH 2000 (Norotos/ARC/No Velcro)";
-		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry\gear\head\data\bare_mich_od_norotos_arc_co.paa", "\tsp_rhs_mich\tex\rail_green_ca.paa"};
+	class rhsusf_mich_bare_norotos_arc_light: rhsusf_mich_bare_norotos_arc
+	{
+		displayName = "MICH 2000 (Light/Norotos/ARC)";
+		picture = "\tsp_rhs_mich\ui\mich_light.paa";
+		hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_light.paa","\tsp_rhs_mich\tex\rail_light_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
 	};
-	class rhsusf_mich_bare_norotos_arc_headset_novelcro: rhsusf_mich_bare_norotos_arc_headset {displayName = "MICH 2000 (Norotos/ARC/Headset/No Velcro)";};
-	class rhsusf_mich_bare_norotos_arc_alt_novelcro: rhsusf_mich_bare_norotos_arc_alt {displayName = "MICH 2000 (Norotos/ARC/Alt/No Velcro)";};
-	class rhsusf_mich_bare_norotos_arc_alt_headset_novelcro: rhsusf_mich_bare_norotos_arc_alt_headset {displayName = "MICH 2000 (Norotos/ARC/Alt/Headset/No Velcro)";};
-	*/
-	//class rhsusf_mich_bare_norotos_coyote: rhsusf_mich_bare_norotos {displayName = "MICH 2000 (Coyote, Norotos)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_coyote.paa","\tsp_rhs_mich\tex\rail_coyote_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	//class rhsusf_mich_bare_norotos_headset_coyote: rhsusf_mich_bare_norotos_headset {displayName = "MICH 2000 (Coyote, Norotos/Headset)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_coyote.paa","\tsp_rhs_mich\tex\rail_coyote_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	//class rhsusf_mich_bare_norotos_alt_coyote: rhsusf_mich_bare_norotos_alt {displayName = "MICH 2000 (Coyote, Norotos/Alt)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_coyote.paa","\tsp_rhs_mich\tex\rail_coyote_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	//class rhsusf_mich_bare_norotos_alt_headset_coyote: rhsusf_mich_bare_norotos_alt_headset {displayName = "MICH 2000 (Coyote, Norotos/Alt/Headset)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_coyote.paa","\tsp_rhs_mich\tex\rail_coyote_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	class rhsusf_mich_bare_norotos_arc_coyote: rhsusf_mich_bare_norotos_arc {displayName = "MICH 2000 (Coyote, Norotos/ARC)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_coyote.paa","\tsp_rhs_mich\tex\rail_coyote_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	//class rhsusf_mich_bare_norotos_arc_headset_coyote: rhsusf_mich_bare_norotos_arc_headset {displayName = "MICH 2000 (Coyote, Norotos/ARC/Headset)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_coyote.paa","\tsp_rhs_mich\tex\rail_coyote_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	class rhsusf_mich_bare_norotos_arc_alt_coyote: rhsusf_mich_bare_norotos_arc_alt {displayName = "MICH 2000 (Coyote, Norotos/ARC/Alt)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_coyote.paa","\tsp_rhs_mich\tex\rail_coyote_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	//class rhsusf_mich_bare_norotos_arc_alt_headset_coyote: rhsusf_mich_bare_norotos_arc_alt_headset {displayName = "MICH 2000 (Coyote, Norotos/ARC/Alt/Headset)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_coyote.paa","\tsp_rhs_mich\tex\rail_coyote_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-
-	//class rhsusf_mich_bare_norotos_light: rhsusf_mich_bare_norotos {displayName = "MICH 2000 (Light, Norotos)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_light.paa","\tsp_rhs_mich\tex\rail_light_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	//class rhsusf_mich_bare_norotos_headset_light: rhsusf_mich_bare_norotos_headset {displayName = "MICH 2000 (Light, Norotos/Headset)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_light.paa","\tsp_rhs_mich\tex\rail_light_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	//class rhsusf_mich_bare_norotos_alt_light: rhsusf_mich_bare_norotos_alt {displayName = "MICH 2000 (Light, Norotos/Alt)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_light.paa","\tsp_rhs_mich\tex\rail_light_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	//class rhsusf_mich_bare_norotos_alt_headset_light: rhsusf_mich_bare_norotos_alt_headset {displayName = "MICH 2000 (Light, Norotos/Alt/Headset)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_light.paa","\tsp_rhs_mich\tex\rail_light_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	class rhsusf_mich_bare_norotos_arc_light: rhsusf_mich_bare_norotos_arc {displayName = "MICH 2000 (Light, Norotos/ARC)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_light.paa","\tsp_rhs_mich\tex\rail_light_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	//class rhsusf_mich_bare_norotos_arc_headset_light: rhsusf_mich_bare_norotos_arc_headset {displayName = "MICH 2000 (Light, Norotos/ARC/Headset)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_light.paa","\tsp_rhs_mich\tex\rail_light_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	class rhsusf_mich_bare_norotos_arc_alt_light: rhsusf_mich_bare_norotos_arc_alt {displayName = "MICH 2000 (Light, Norotos/ARC/Alt)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_light.paa","\tsp_rhs_mich\tex\rail_light_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-	//class rhsusf_mich_bare_norotos_arc_alt_headset_light: rhsusf_mich_bare_norotos_arc_alt_headset {displayName = "MICH 2000 (Light, Norotos/ARC/Alt/Headset)"; hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_light.paa","\tsp_rhs_mich\tex\rail_light_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};};
-
-	class rhsusf_mich_bare_norotos_coyote: rhsusf_mich_bare_norotos	{
-		displayName = "MICH 2000 (Coyote, Norotos)";
+	class rhsusf_mich_bare_norotos_arc_alt_light: rhsusf_mich_bare_norotos_arc_alt
+	{
+		displayName = "MICH 2000 (Light/Norotos/ARC/Alt)";
+		picture = "\tsp_rhs_mich\ui\mich_light_alt.paa";
+		hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_light.paa","\tsp_rhs_mich\tex\rail_light_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_arc_coyote: rhsusf_mich_bare_norotos_arc
+	{
+		displayName = "MICH 2000 (Coyote/Norotos/ARC)";
+		picture = "\tsp_rhs_mich\ui\mich_coyote.paa";
+		hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_coyote.paa","\tsp_rhs_mich\tex\rail_coyote_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_arc_alt_coyote: rhsusf_mich_bare_norotos_arc_alt
+	{
+		displayName = "MICH 2000 (Coyote/Norotos/ARC/Alt)";
+		picture = "\tsp_rhs_mich\ui\mich_coyote_alt.paa";
+		hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_coyote.paa","\tsp_rhs_mich\tex\rail_coyote_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_coyote: rhsusf_mich_bare_norotos
+	{
+		displayName = "MICH 2000 (Coyote/Norotos)";
+		picture = "\tsp_rhs_mich\ui\mich_coyote.paa";
 		hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_bare.paa","\tsp_rhs_mich\tex\rail_coyote_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
 	};
-	class rhsusf_mich_bare_norotos_alt_coyote: rhsusf_mich_bare_norotos_alt	{
-		displayName = "MICH 2000 (Coyote, Norotos/Alt)";
+	class rhsusf_mich_bare_norotos_alt_coyote: rhsusf_mich_bare_norotos_alt
+	{
+		displayName = "MICH 2000 (Coyote/Norotos/Alt)";
+		picture = "\tsp_rhs_mich\ui\mich_coyote_alt.paa";
 		hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_bare.paa","\tsp_rhs_mich\tex\rail_coyote_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
 	};
+	class rhsusf_mich_bare_norotos_coyote_headset: rhsusf_mich_bare_norotos_headset
+	{
+		displayName = "MICH 2000 (Coyote/Norotos/Headset)";
+		picture = "\tsp_rhs_mich\ui\mich_coyote_alt.paa";
+		hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_bare.paa","\tsp_rhs_mich\tex\rail_coyote_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\bowman_base_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_green: rhsusf_mich_bare_norotos
+	{
+		displayName = "MICH 2000 (Green/Norotos)";
+		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry\gear\head\data\bare_mich_norotos_od_co.paa","\tsp_rhs_mich\tex\rail_green_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_alt_green: rhsusf_mich_bare_norotos_alt
+	{
+		displayName = "MICH 2000 (Green/Norotos/Alt)";
+		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry\gear\head\data\bare_mich_norotos_od_co.paa","\tsp_rhs_mich\tex\rail_green_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_green_headset: rhsusf_mich_bare_norotos_headset
+	{
+		displayName = "MICH 2000 (Green/Norotos/Headset)";
+		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry\gear\head\data\bare_mich_norotos_od_co.paa","\tsp_rhs_mich\tex\rail_green_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\bowman_base_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_arc_green: rhsusf_mich_bare_norotos_arc
+	{
+		displayName = "MICH 2000 (Green/Norotos/ARC)";
+		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry\gear\head\data\bare_mich_od_norotos_arc_co.paa","\tsp_rhs_mich\tex\rail_green_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_arc_alt_green: rhsusf_mich_bare_norotos_arc_alt
+	{
+		displayName = "MICH 2000 (Green/Norotos/ARC/Alt)";
+		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry\gear\head\data\bare_mich_od_norotos_arc_co.paa","\tsp_rhs_mich\tex\rail_green_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_arc_green_headset: rhsusf_mich_bare_norotos_arc_headset
+	{
+		displayName = "MICH 2000 (Green/Norotos/ARC/Headset)";
+		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry\gear\head\data\bare_mich_od_norotos_arc_co.paa","\tsp_rhs_mich\tex\rail_green_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\bowman_base_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_black: rhsusf_mich_bare_norotos
+	{
+		displayName = "MICH 2000 (Black/Norotos)";
+		picture = "\tsp_rhs_mich\ui\mich_black.paa";
+		hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_black.paa","\tsp_rhs_mich\tex\rail_black_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_alt_black: rhsusf_mich_bare_norotos_alt
+	{
+		displayName = "MICH 2000 (Black/Norotos/Alt)";
+		picture = "\tsp_rhs_mich\ui\mich_black_alt.paa";
+		hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_black.paa","\tsp_rhs_mich\tex\rail_black_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_black_headset: rhsusf_mich_bare_norotos_headset
+	{
+		displayName = "MICH 2000 (Black/Norotos/Headset)";
+		picture = "\tsp_rhs_mich\ui\mich_black_alt.paa";
+		hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_black.paa","\tsp_rhs_mich\tex\rail_black_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\bowman_base_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_arc_black: rhsusf_mich_bare_norotos_arc
+	{
+		displayName = "MICH 2000 (Black/Norotos/ARC)";
+		picture = "\tsp_rhs_mich\ui\mich_black.paa";
+		hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_black_arc.paa","\tsp_rhs_mich\tex\rail_black_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_arc_alt_black: rhsusf_mich_bare_norotos_arc_alt
+	{
+		displayName = "MICH 2000 (Black/Norotos/ARC/Alt)";
+		picture = "\tsp_rhs_mich\ui\mich_black_alt.paa";
+		hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_black_arc.paa","\tsp_rhs_mich\tex\rail_black_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+	};
+	class rhsusf_mich_bare_norotos_arc_black_headset: rhsusf_mich_bare_norotos_arc_headset
+	{
+		displayName = "MICH 2000 (Black/Norotos/ARC/Headset)";
+		picture = "\tsp_rhs_mich\ui\mich_black_alt.paa";
+		hiddenSelectionsTextures[] = {"\tsp_rhs_mich\tex\mich_black_arc.paa","\tsp_rhs_mich\tex\rail_black_ca.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\bowman_base_co.paa"};
+	};
+};
+class cfgMods
+{
+	author = "AHHHHHHHHHHHHHHH";
+	timepacked = "1755462899";
 };
